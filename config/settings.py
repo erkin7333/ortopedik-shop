d6a82b361821bab0525b84896782b7310c9d1910
erkin7333 <erkinnajimov90@gmail.com>
2023-04-04 14:48:11 +0500

    Static

--- config/settings.py
+++ config/settings.py
@@ -154,11 +154,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets/static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static', )
-]
 
 AUTH_USER_MODEL = 'account.User'
 
